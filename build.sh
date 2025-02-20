f8443317abd2d86df33bcd58e084071099f41dc5
Shruthi-yuv <shruthishuthi93214@gmail.com>
2025-02-20 10:16:46 +0530

    second commit

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-# Apply database migrations
-python manage.py migrate
+#exit on error
+set -0 errexit
+
+#modify this line as needed for your pacakage manager(pip,poetry,etc.)
+pip install -r requirements.txt
+
 # Collect static files
 python manage.py collectstatic --noinput
+
+# Apply database migrations
+python manage.py migrate
